f98ceaaec7fcf2b6a28a4c573224d73cf88aa2d4
Paul Springett <paul@simpleweb.co.uk>
2011-08-05 12:28:09 +0100

    Adding markdown live preview

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+  
+  // live markdown preview
+  var showdown = new Showdown.converter();
+  var prev_text = "";
+  var update_live_preview = function() {
+      var input_text = $('#idea_content').val();
+      if (input_text != prev_text) {
+          var text = $("<div>"+ showdown.makeHtml(input_text) +"</div>");
+          console.log(text);
+          $('div#live-preview').html(text);
+          prev_text = input_text;
+      }
+  }
+  setInterval(update_live_preview, 300);
+});
